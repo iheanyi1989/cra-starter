ae7e37b07e7739d2e14763093c7f341771fa0464
Iheanyi Onwubiko <ioncloudjourney@gmail.com>
2023-02-11 14:51:43 -0500

    test prod

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - No, thanks!
+          Learn React - No, Push to prod!
         </a>
       </header>
     </div>
